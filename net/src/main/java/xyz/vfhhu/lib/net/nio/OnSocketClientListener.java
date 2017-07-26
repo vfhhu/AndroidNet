cdd0a133a87f8d74123c94952d4c44e21b32ab52
vfhhu <newvfhhu@yahoo.com.tw>
2017-07-26 11:00:14 +0800

    new

---
+++ net/src/main/java/xyz/vfhhu/lib/net/nio/OnSocketClientListener.java
@@ -0,0 +1,14 @@
+package xyz.vfhhu.lib.net.nio;
+
+/**
+ * Created by leo on 2017/6/22.
+ */
+
+public interface OnSocketClientListener {
+    void onSend(String src, SocketClient client);
+    void onData(String src, SocketClient client);
+    void onConnected(SocketClient client);
+    void onClosed(SocketClient client);
+    void onError(String err, SocketClient client);
+
+}
